884f6d57cc421e5279c5b7c492888e1c127aa65c
peternguyen <nguye5he@mail.uc.edu>
2025-12-11 00:14:58 -0500

    complete ui

--- src/RandProductMaker.java
+++ src/RandProductMaker.java
@@ -1,5 +1,10 @@
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+
+import static java.lang.System.exit;
 
 public class RandProductMaker extends JFrame {
     JPanel mainPnl;
@@ -30,7 +35,16 @@ public class RandProductMaker extends JFrame {
         createTitlePnl();
         mainPnl.add(titlePnl, BorderLayout.NORTH);
 
+        createContentPnl();
+        mainPnl.add(contentPnl, BorderLayout.CENTER);
+
+        createButtonPnl();
+        mainPnl.add(buttonPnl, BorderLayout.SOUTH);
+
         add(mainPnl);
+        setSize(400,400);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     private void createTitlePnl(){
@@ -41,17 +55,47 @@ public class RandProductMaker extends JFrame {
 
     private void createContentPnl(){
         contentPnl = new JPanel();
-        contentPnl.setLayout(new BoxLayout(contentPnl, BoxLayout.Y_AXIS));
+        SpringLayout layout = new SpringLayout();
+        contentPnl.setLayout(layout);
+
         JLabel nameLbl = new JLabel("Name:");
         JLabel desLbl = new JLabel("Description:");
         JLabel idLbl = new JLabel("ID:");
         JLabel costLbl = new JLabel("Cost:");
         JLabel countLbl = new JLabel("Count:");
-        nameField = new JTextField();
+
+        nameField = new JTextField(15);
         desField = new JTextField(15);
         idField = new JTextField(15);
         costField = new JTextField(15);
         countField = new JTextField(15);
+
+
+        layout.putConstraint(SpringLayout.WEST, nameLbl, 20, SpringLayout.WEST, contentPnl);
+        layout.putConstraint(SpringLayout.NORTH, nameLbl, 10, SpringLayout.NORTH, contentPnl);
+        layout.putConstraint(SpringLayout.WEST, nameField, 20, SpringLayout.EAST, desLbl);
+        layout.putConstraint(SpringLayout.NORTH, nameField, 0, SpringLayout.NORTH, nameLbl);
+
+        layout.putConstraint(SpringLayout.WEST, desLbl, 20, SpringLayout.WEST, contentPnl);
+        layout.putConstraint(SpringLayout.NORTH, desLbl, 60, SpringLayout.NORTH, contentPnl);
+        layout.putConstraint(SpringLayout.WEST, desField, 20, SpringLayout.EAST, desLbl);
+        layout.putConstraint(SpringLayout.NORTH, desField, 0, SpringLayout.NORTH, desLbl);
+
+        layout.putConstraint(SpringLayout.WEST, idLbl, 20, SpringLayout.WEST, contentPnl);
+        layout.putConstraint(SpringLayout.NORTH, idLbl, 110, SpringLayout.NORTH, contentPnl);
+        layout.putConstraint(SpringLayout.WEST, idField, 20, SpringLayout.EAST, desLbl);
+        layout.putConstraint(SpringLayout.NORTH, idField, 0, SpringLayout.NORTH, idLbl);
+
+        layout.putConstraint(SpringLayout.WEST, costLbl, 20, SpringLayout.WEST, contentPnl);
+        layout.putConstraint(SpringLayout.NORTH, costLbl, 160, SpringLayout.NORTH, contentPnl);
+        layout.putConstraint(SpringLayout.WEST, costField, 20, SpringLayout.EAST, desLbl);
+        layout.putConstraint(SpringLayout.NORTH, costField, 0, SpringLayout.NORTH, costLbl);
+
+        layout.putConstraint(SpringLayout.WEST, countLbl, 20, SpringLayout.WEST, contentPnl);
+        layout.putConstraint(SpringLayout.NORTH, countLbl, 210, SpringLayout.NORTH, contentPnl);
+        layout.putConstraint(SpringLayout.WEST, countField, 20, SpringLayout.EAST, desLbl);
+        layout.putConstraint(SpringLayout.NORTH, countField, 0, SpringLayout.NORTH, countLbl);
+
         contentPnl.add(nameLbl);
         contentPnl.add(nameField);
         contentPnl.add(desLbl);
@@ -62,5 +106,15 @@ public class RandProductMaker extends JFrame {
         contentPnl.add(costField);
         contentPnl.add(countLbl);
         contentPnl.add(countField);
+    }
+
+    private void createButtonPnl(){
+        buttonPnl = new JPanel();
+        addBtn = new JButton("Add");
+        quitBtn = new JButton("Quit");
+
+        quitBtn.addActionListener((ActionEvent e)->exit(0));
+        buttonPnl.add(addBtn);
+        buttonPnl.add(quitBtn);
     }
 }
